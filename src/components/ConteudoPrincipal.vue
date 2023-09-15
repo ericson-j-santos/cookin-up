ba0639dc5ff11abf3d3f4862337f1d6df20cbad2
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-09-15 11:12:53 -0300

    Vídeo: Importando dados

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,10 +1,13 @@
 <script lang="ts">
+import SelecionarIngredientes from './SelecionarIngredientes.vue';
+
 export default {
-  data() {
-    return {
-      ingredientes: ['Alho', 'Manteiga', 'Orégano']
-    }
-  }
+    data() {
+        return {
+            ingredientes: ['Alho', 'Manteiga', 'Orégano']
+        };
+    },
+    components: { SelecionarIngredientes }
 }
 </script>
 
@@ -26,6 +29,8 @@ export default {
         Sua lista está vazia, selecione ingredientes para iniciar.
       </p>
     </section>
+
+    <SelecionarIngredientes />
   </main>
 </template>
 
