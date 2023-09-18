d906db6274aae19a41769b9d88b1048394aed92b
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-09-18 09:47:48 -0300

    Desafio: criando componente SuaLista

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import SelecionarIngredientes from './SelecionarIngredientes.vue';
+import SuaLista from './SuaLista.vue';
 import Tag from './Tag.vue';
 
 export default {
@@ -8,28 +9,13 @@ export default {
       ingredientes: ['Alho', 'Manteiga', 'Orégano']
     };
   },
-  components: { SelecionarIngredientes, Tag }
+  components: { SelecionarIngredientes, Tag, SuaLista }
 }
 </script>
 
 <template>
   <main class="conteudo-principal">
-    <section>
-      <span class="subtitulo-lg sua-lista-texto">
-        Sua lista:
-      </span>
-
-      <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li v-for="ingrediente in ingredientes" :key="ingrediente">
-          <Tag :texto="ingrediente" ativa />
-        </li>
-      </ul>
-
-      <p v-else class="paragrafo lista-vazia">
-        <img src="../assets/imagens/icones/lista-vazia.svg" alt="Ícone de pesquisa">
-        Sua lista está vazia, selecione ingredientes para iniciar.
-      </p>
-    </section>
+    <SuaLista :ingredientes="ingredientes" />
 
     <SelecionarIngredientes />
   </main>
